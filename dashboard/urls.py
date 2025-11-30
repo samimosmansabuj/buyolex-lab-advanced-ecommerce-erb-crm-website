91fdf5e7a70a5dda756c683d7171e2fdfe14d6cc
samimosmansabuj <samim.o.sabuj01@gmail.com>
2025-12-01 03:22:04 +0600

    dashboard template foldering!!!

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, product_list, add_product, category_list, add_category
 
 urlpatterns = [
-    path('', dashboard, name='dashboard')
+    path('', dashboard, name='dashboard'),
+    path('product-list/', product_list, name='product_list'),
+    path('product-add/', add_product, name='product_add'),
+    path('media-center/', product_list, name='media_center'),
+
+    path('category-list/', category_list, name='category_list'),
+    path('category-add/', add_category, name='category_add'),
 ]
