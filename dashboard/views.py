815f6f29e453d2671e9b63c5edad83135cbb57ac
SumiyaAkter701 <sumiyaakter.9586@gmail.com>
2026-02-23 12:46:48 +0600

    stite_app, models etc work for steadfast

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 from django.db import transaction
 from http import HTTPStatus
 from orders.models import Order
-from orders.utix import ORDER_STATUS
+from orders.utix import ORDER_STATUS, DELIVERY_TYPE
 import json
 from django.db import transaction
 from django.http import QueryDict
@@ -22,6 +22,9 @@ from django.utils import timezone
 from django.db.models.functions import Coalesce
 from django.db.models import Sum, F, Value, DecimalField
 from django.db.models.functions import Coalesce
+from orders.utils import SteadFastParcelAPI
+from site_app.models import DeliveryOption
+
 
 class DashboardView(LoginRequiredMixin, View):
     login_url = 'admin_login'
@@ -459,99 +462,99 @@ class OrderInvoiceView(View):
         return redirect(request.META.get('HTTP_REFERER'))
 
 
-# class OrderDeliveryOptionSubmitView(LoginRequiredMixin, View):
-#     model = Order
-#     login_url = "admin_login"
+class OrderDeliveryOptionSubmitView(LoginRequiredMixin, View):
+    model = Order
+    login_url = "admin_login"
 
-#     def get_order(self, id):
-#         try:
-#             return get_object_or_404(Order, id=id)
-#         except Exception as e:
-#             raise Exception(str(e))
+    def get_order(self, id):
+        try:
+            return get_object_or_404(Order, id=id)
+        except Exception as e:
+            raise Exception(str(e))
     
-#     def get_order_data(self, order):
-#         email = (order.customer.email or order.customer.user.email if order.customer.user else None) or None
-#         whatsapp = order.customer.whatsapp if order.customer.whatsapp else None
-#         data = {
-#             "invoice": order.order_id,
-#             "recipient_name": order.customer.name,
-#             "recipient_phone": order.customer.phone,
-#             "recipient_address": order.shipping_address,
-#             "cod_amount": float(order.total_cost),
-#             "note": order.note,
-#             "total_lot": order.order_items.count(),
-#             "delivery_type": 1 if order.delivery_type == DELIVERY_TYPE.PICKUP else 0,
-#         }
-#         if whatsapp:
-#             data["alternative_phone"] = whatsapp
-#         if email:
-#             data["recipient_email"] = email
-#         return data
+    def get_order_data(self, order):
+        email = (order.customer.email or order.customer.user.email if order.customer.user else None) or None
+        whatsapp = order.customer.whatsapp if order.customer.whatsapp else None
+        data = {
+            "invoice": order.order_id,
+            "recipient_name": order.customer.name,
+            "recipient_phone": order.customer.phone,
+            "recipient_address": order.shipping_address,
+            "cod_amount": float(order.total_cost),
+            "note": order.note,
+            "total_lot": order.order_items.count(),
+            "delivery_type": 1 if order.delivery_type == DELIVERY_TYPE.PICKUP else 0,
+        }
+        if whatsapp:
+            data["alternative_phone"] = whatsapp
+        if email:
+            data["recipient_email"] = email
+        return data
     
-#     def steadfast_response(self, logistics_partner, order):
-#         order_data = self.get_order_data(order)
-#         steadfast = SteadFastParcelAPI(logistics_partner.id)
-#         return steadfast.create_order(order_data)
-#         # return {
-#         #     "status": 200,
-#         #     "message": "Consignment has been created successfully.",
-#         #     "consignment": {
-#         #         "consignment_id": 1424107,
-#         #         "invoice": "Aa12-das4",
-#         #         "tracking_code": "15BAEB8A",
-#         #         "recipient_name": "John Smith",
-#         #         "recipient_phone": "01234567890",
-#         #         "recipient_address": "Fla# A1,House# 17/1, Road# 3/A, Dhanmondi,Dhaka-1209",
-#         #         "cod_amount": 1060,
-#         #         "status": "in_review",
-#         #         "note": "Deliver within 3PM",
-#         #         "created_at": "2021-03-21T07:05:31.000000Z",
-#         #         "updated_at": "2021-03-21T07:05:31.000000Z",
-#         #     },
-#         # }
+    def steadfast_response(self, logistics_partner, order):
+        order_data = self.get_order_data(order)
+        steadfast = SteadFastParcelAPI(logistics_partner.id)
+        return steadfast.create_order(order_data)
+        # return {
+        #     "status": 200,
+        #     "message": "Consignment has been created successfully.",
+        #     "consignment": {
+        #         "consignment_id": 1424107,
+        #         "invoice": "Aa12-das4",
+        #         "tracking_code": "15BAEB8A",
+        #         "recipient_name": "John Smith",
+        #         "recipient_phone": "01234567890",
+        #         "recipient_address": "Fla# A1,House# 17/1, Road# 3/A, Dhanmondi,Dhaka-1209",
+        #         "cod_amount": 1060,
+        #         "status": "in_review",
+        #         "note": "Deliver within 3PM",
+        #         "created_at": "2021-03-21T07:05:31.000000Z",
+        #         "updated_at": "2021-03-21T07:05:31.000000Z",
+        #     },
+        # }
     
-#     def get_logistics_partners(self, data):
-#         logistics_partner_id = data.get("logistics_partner")
-#         return DeliveryOption.objects.get(id=logistics_partner_id)
+    def get_logistics_partners(self, data):
+        logistics_partner_id = data.get("logistics_partner")
+        return DeliveryOption.objects.get(id=logistics_partner_id)
     
-#     def return_response(self, success, message, data=None, status=None) -> JsonResponse:
-#         response = {
-#             "success": success,
-#             "message": message,
-#         }
-#         if data:
-#             courier_name = data.courier.name if data.courier else None
-#             courier_type = data.courier.type if data.courier.type else None
-#             courier = f"{courier_name} ({courier_type})" if courier_name and courier_type else courier_name or courier_type or None
-#             response["data"] = {
-#                 "id": data.id,
-#                 "courier": courier,
-#                 "tracking_number": data.tracking_number,
-#                 "status": data.status,
-#                 "updated_at": data.updated_at,
-#                 "created_at": data.updated_at
-#             }
-#         return JsonResponse(
-#             response,
-#             status=status,
-#         )
+    def return_response(self, success, message, data=None, status=None) -> JsonResponse:
+        response = {
+            "success": success,
+            "message": message,
+        }
+        if data:
+            courier_name = data.courier.name if data.courier else None
+            courier_type = data.courier.type if data.courier.type else None
+            courier = f"{courier_name} ({courier_type})" if courier_name and courier_type else courier_name or courier_type or None
+            response["data"] = {
+                "id": data.id,
+                "courier": courier,
+                "tracking_number": data.tracking_number,
+                "status": data.status,
+                "updated_at": data.updated_at,
+                "created_at": data.updated_at
+            }
+        return JsonResponse(
+            response,
+            status=status,
+        )
     
-#     def post(self, request, *args: str, **kwargs):
-#         try:
-#             with transaction.atomic():
-#                 data = json.loads(request.body)
-#                 logistics_partner = self.get_logistics_partners(data)
-#                 order = self.get_order(kwargs.get("pk"))
-#                 steadfast_response = self.steadfast_response(logistics_partner, order)
-#                 print("steadfast_response: ", steadfast_response)
-#                 if steadfast_response.get("status") == 200:
-#                     order_shipped_data = order.shipments.create(
-#                         courier=logistics_partner,
-#                         tracking_number=steadfast_response["consignment"]["consignment_id"],
-#                         status=steadfast_response["consignment"]["status"],
-#                     )
-#                     return self.return_response(True, steadfast_response.get("message"), data=order_shipped_data, status=HTTPStatus.OK)
-#                 else:
-#                     return self.return_response(False, steadfast_response.get("message"), status=HTTPStatus.BAD_REQUEST)
-#         except Exception as e:
-#             return self.return_response(False, f"{str(e)}", status=HTTPStatus.BAD_REQUEST)
+    def post(self, request, *args: str, **kwargs):
+        try:
+            with transaction.atomic():
+                data = json.loads(request.body)
+                logistics_partner = self.get_logistics_partners(data)
+                order = self.get_order(kwargs.get("pk"))
+                steadfast_response = self.steadfast_response(logistics_partner, order)
+                print("steadfast_response: ", steadfast_response)
+                if steadfast_response.get("status") == 200:
+                    order_shipped_data = order.shipments.create(
+                        courier=logistics_partner,
+                        tracking_number=steadfast_response["consignment"]["consignment_id"],
+                        status=steadfast_response["consignment"]["status"],
+                    )
+                    return self.return_response(True, steadfast_response.get("message"), data=order_shipped_data, status=HTTPStatus.OK)
+                else:
+                    return self.return_response(False, steadfast_response.get("message"), status=HTTPStatus.BAD_REQUEST)
+        except Exception as e:
+            return self.return_response(False, f"{str(e)}", status=HTTPStatus.BAD_REQUEST)
